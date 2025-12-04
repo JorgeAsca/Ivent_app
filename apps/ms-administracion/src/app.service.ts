00e033f5e3eee3e3245458e0f290925d8a5ecae9
jorgeasca <jascola171@gamil.com>
2025-12-04 14:12:54 +0100

    Inserciones de ms-administracion

--- apps/ms-administracion/src/app.service.ts
+++ apps/ms-administracion/src/app.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
+import { Pool } from 'pg'; 
+import { PrismaPg } from '@prisma/adapter-pg'; 
 
 @Injectable()
 export class AppService extends PrismaClient implements OnModuleInit {
   
  constructor() {
+    
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+
+    
+    const adapter = new PrismaPg(pool);
+
+    
     super({
+      adapter,
       log: ['query', 'info', 'warn', 'error'],
     });
   }
