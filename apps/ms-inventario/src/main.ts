d688a77f2df7c826dd4c7454bac4131131d4d5a0
jorgeasca <jascola171@gamil.com>
2025-12-14 16:13:00 +0100

    ms-inventario

--- apps/ms-inventario/src/main.ts
+++ apps/ms-inventario/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Ms-Inventario');
+  
+  
+  const port = process.env.inventario_PORT ? parseInt(process.env.inventario_PORT) : 3002;
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: port,
+    },
+  });
+
+  await app.listen();
+  logger.log(`Microservicio Inventario corriendo en puerto ${port}`);
 }
 bootstrap();
