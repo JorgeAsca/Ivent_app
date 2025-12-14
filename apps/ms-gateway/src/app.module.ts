934d9416d962d021981b906ff55e32683aef1466
jorgeasca <jascola171@gamil.com>
2025-12-14 10:47:05 +0100

    Corrigiendo errores de puertos y host de ms-administracion

--- apps/ms-gateway/src/app.module.ts
+++ apps/ms-gateway/src/app.module.ts
@@ -2,19 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // <--- Importante
 
 @Module({
   imports: [
-    ClientsModule.register([
+    // Añadimos ConfigModule para leer el .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    
+    
+    ClientsModule.registerAsync([
       {
-        name: 'ADMINISTRACION_SERVICE', 
-        transport: Transport.TCP,
-        options: {
-          host: '127.0.0.1', 
-          port: 3001,
-        },
+        name: 'ADMINISTRACION_SERVICE',
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            // Leemos el host del .env
+            host: configService.get('MS_ADMINISTRACION_HOST'), 
+            // Leemos el puerto interno (3000)
+            port: configService.get('MS_ADMINISTRACION_PORT'), 
+          },
+        }),
+        inject: [ConfigService],
       },
-      
     ]),
   ],
   controllers: [AppController],
