67a53d77d077a25d8bdac3c31faae2ef5fafa3a3
jorgeasca <jascola171@gamil.com>
2025-12-16 00:28:10 +0100

    ms-inventario

--- apps/ms-gateway/src/modulos/ms-inventario/ms-inventario.controller.ts
+++ apps/ms-gateway/src/modulos/ms-inventario/ms-inventario.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
-import { CreateProductoDto, INVENTARIO_PATTERNS } from '@app/common';
+// Asegúrate de exportar CreateCategoriaDto en tu librería @app/common
+import { CreateProductoDto, INVENTARIO_PATTERNS, CreateCategoriaDto } from '@app/common'; 
 
 @Controller('inventario')
 export class InventarioController {
@@ -9,6 +10,7 @@ export class InventarioController {
         @Inject('INVENTARIO_SERVICE') private readonly clientInventario: ClientProxy,
     ) { }
 
+    
     @Post('productos')
     createProducto(@Body() createProductoDto: CreateProductoDto) {
         return this.clientInventario.send(INVENTARIO_PATTERNS.CREAR_PRODUCTO, createProductoDto).pipe(
@@ -19,6 +21,21 @@ export class InventarioController {
     @Get('productos')
     findAllProductos() {
         return this.clientInventario.send(INVENTARIO_PATTERNS.LISTAR_PRODUCTOS, {}).pipe(
+            catchError((error) => { throw new RpcException(error); }),
+        );
+    }
+
+
+    @Post('categorias')
+    createCategoria(@Body() createCategoriaDto: CreateCategoriaDto) {
+        return this.clientInventario.send(INVENTARIO_PATTERNS.CREAR_CATEGORIA, createCategoriaDto).pipe(
+            catchError((error) => { throw new RpcException(error); }),
+        );
+    }
+
+    @Get('categorias')
+    findAllCategorias() {
+        return this.clientInventario.send(INVENTARIO_PATTERNS.LISTAR_CATEGORIAS, {}).pipe(
             catchError((error) => { throw new RpcException(error); }),
         );
     }
