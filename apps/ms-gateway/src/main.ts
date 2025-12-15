3b3903d78f71acbb8b96cb2a0bc5f6b5ab541397
jorgeasca <jascola171@gamil.com>
2025-12-16 00:39:00 +0100

    ms-inventario

--- apps/ms-gateway/src/main.ts
+++ apps/ms-gateway/src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common'; // <--- IMPORTAR ESTO
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Gateway');
+
+  // --- AGREGAR ESTAS LÍNEAS ---
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina datos que no estén en el DTO
+      forbidNonWhitelisted: true, // Lanza error si envían datos extra
+      transform: true, // Convierte "10" a 10 automáticamente (vital para tus números)
+    }),
+  );
+  // ----------------------------
   
-  // Definimos el puerto en una variable simple
   const port = 3000; 
-  
   await app.listen(port);
   
-  // Mensaje bonito para la consola
-  console.log(`Gateway escuchando peticiones HTTP en: http://localhost:${port}`);
+  logger.log(`Gateway escuchando peticiones HTTP en: http://localhost:${port}`);
 }
 bootstrap();
