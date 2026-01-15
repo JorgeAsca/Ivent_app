195e3abd026fa4875a32cea8b233c2f21e76b994
jorgeasca <jascola171@gamil.com>
2026-01-15 20:19:36 +0100

    Red de Nats

--- apps/ms-inventario/src/main.ts
+++ apps/ms-inventario/src/main.ts
@@ -2,17 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { envs } from './config/envs'; //
+import { envs } from './config/envs'; 
 
 async function bootstrap() {
   const logger = new Logger('Ms-Inventario');
 
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
+    transport: Transport.NATS,
     options: {
-      host: '0.0.0.0',
-      port: envs.port,
+      servers: envs.natsServers,
     },
   });
 
