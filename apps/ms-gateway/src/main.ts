1d86ad8e277ce4809e303577b6dec2be19415900
jorgeasca <jascola171@gamil.com>
2026-03-18 09:28:08 +0100

    Nuxt

--- apps/ms-gateway/src/main.ts
+++ apps/ms-gateway/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Permite peticiones desde tu Nuxt local
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
