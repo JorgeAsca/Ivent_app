00bbb49b27a64a7f5309bd1e9a0237100d8bb10a
jorgeasca <jascola171@gamil.com>
2026-03-23 09:36:53 +0100

    CORDS

--- apps/ms-gateway/src/main.ts
+++ apps/ms-gateway/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'true',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
