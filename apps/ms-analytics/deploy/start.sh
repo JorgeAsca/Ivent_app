f9fd09ae2cccf913a94af795e8d541120b21e49b
jorgeasca <jascola171@gamil.com>
2026-05-07 10:30:26 +0200

    fix nestcli

--- apps/ms-analytics/deploy/start.sh
+++ apps/ms-analytics/deploy/start.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 set -e
 
-# Logs de arranque
-INFORME=/root/logs/informe.log
-mkdir -p /root/logs
-echo "*** Arrancando ms-analytics ***" > ${INFORME}
+# Ya no instalamos nada aquí, Docker ya lo hizo
+echo "Arrancando microservicio ms-analytics..."
 
-# Instalación de herramientas
-npm install -g pnpm
-pnpm config set ignore-scripts false
-
-# Instalación de dependencias
-echo "Instalando dependencias..." >> ${INFORME}
-pnpm install --no-frozen-lockfile
-
-# Forzar la compilación para generar la carpeta dist con las dependencias vinculadas
-echo "Compilando ms-analytics..." >> ${INFORME}
-npx nest build ms-analytics
-
-echo "Iniciando ms-analytics..." >> ${INFORME}
-# Usamos la ruta completa del monorepo que confirmamos antes
+# Ejecutamos directamente el archivo compilado
 exec node dist/apps/ms-analytics/src/main.js
