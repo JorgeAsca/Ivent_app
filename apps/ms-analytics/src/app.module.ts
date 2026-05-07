aed54b09ced97214628be11c6d7d90a38d9bf384
jorgeasca <jascola171@gamil.com>
2026-05-07 13:12:45 +0200

    fix nestcli

--- apps/ms-analytics/src/app.module.ts
+++ apps/ms-analytics/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { IndicadoresInventarioModule } from './indicadores-inventario/indicadores-inventario.module';
 import { HistorialVentasModule } from './historial-ventas/historial-ventas.module';
 import { RendimientoModule } from './rendimiento/rendimiento.module';
@@ -8,19 +8,23 @@ import { RendimientoModule } from './rendimiento/rendimiento.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'db-analytics',
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USER,      // <--- Esta es la clave
-      password: process.env.DB_PASSWORD,  // <--- Esta es la clave
-      database: process.env.DB_NAME,      // <--- Esta es la clave
-      autoLoadEntities: true,
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST') || 'db-analytics',
+        port: configService.get<number>('DB_PORT') || 5432,
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     IndicadoresInventarioModule,
     HistorialVentasModule,
     RendimientoModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
