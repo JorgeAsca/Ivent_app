9872544b19979622156b91bc6511d82112a61619
jorgeasca <jascola171@gamil.com>
2026-05-07 10:59:14 +0200

    fix nestcli

--- apps/ms-analytics/deploy/start.sh
+++ apps/ms-analytics/deploy/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-
-# Ya no instalamos nada aquí, Docker ya lo hizo
 echo "Arrancando microservicio ms-analytics..."
-
-# Ejecutamos directamente el archivo compilado
 exec node dist/apps/ms-analytics/src/main.js
