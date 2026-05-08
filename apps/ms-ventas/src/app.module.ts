a19aa41eacc7ae30fd6aa82e2eb4ed236da11436
gabriel72188 <gabri72188340@gmail.com>
2026-05-08 18:07:39 +0200

    CONFIGURACION OMITIDA POR BREVEDAD XDDDDDD

--- apps/ms-ventas/src/app.module.ts
+++ apps/ms-ventas/src/app.module.ts
@@ -6,11 +6,21 @@ import { ProveedoresModule } from './proveedores/proveedores.module';
 import { DireccionesModule } from './direcciones/direcciones.module';
 import { ContactosModule } from './contactos/contactos.module';
 import { ReglasPedidoModule } from './reglas-pedido/reglas-pedido.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // Configuración de DB omitida por brevedad...
-
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME || 'db_ventas',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     // Registro del Barril de Datos
     ClientsModule.register([
       {
