aa634d0fd7785193b63d62716553af7c15f46859
gabriel72188 <gabri72188340@gmail.com>
2026-05-08 12:02:31 +0200

    cambio de prueba para ve si se ñevanta bien el ms-configuracion

--- apps/ms-configuracion/deploy/start.sh
+++ apps/ms-configuracion/deploy/start.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-set -e
-
-INFORME=/root/logs/informe.log
-mkdir -p /root/logs
-
-echo "*** Despliegue - \$\{MICROSERVICIO\} ***" > \$\{INFORME\}
-
-echo "Instalando herramientas globales (pnpm, tsx)..." >> \$\{INFORME\}
-npm install -g pnpm
-npm install -g tsx
-
-pnpm config set ignore-scripts false
-
-echo "Instalando dependencias..." >> \$\{INFORME\}
-
-pnpm install --frozen-lockfile
-
-echo "Iniciando \$\{MICROSERVICIO\}..." >> \$\{INFORME\}
-exec pnpm run start:dev \$\{MICROSERVICIO\}
+#!/bin/sh
+# Execute the compiled application
+node dist/apps/ms-configuracion/src/main.js
