a75e8c376562d227fad87c78bffcf4c99bb5831d
gabriel72188 <gabri72188340@gmail.com>
2026-05-08 15:41:51 +0200

    arreglo para el ms q usa prisma

--- apps/ms-administracion/deploy/start.sh
+++ apps/ms-administracion/deploy/start.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Starting microservice ms-administracion..."
-# Temporarily using the nested path found by find
-exec node dist/apps/ms-administracion/apps/ms-administracion/src/main.js
+exec node dist/apps/ms-administracion/main.js
