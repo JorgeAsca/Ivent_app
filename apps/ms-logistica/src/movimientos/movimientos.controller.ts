2906cf7d26d5bd178c374061cb35a091475d5300
jorgeasca <jascola171@gamil.com>
2026-05-19 18:01:09 +0200

    Jenkis fix

--- apps/ms-logistica/src/movimientos/movimientos.controller.ts
+++ apps/ms-logistica/src/movimientos/movimientos.controller.ts
@@ -7,11 +7,11 @@ import { CreateMovimientoDto } from './dto/create-movimiento.dto';
 export class MovimientosController {
     constructor(private readonly movimientosService: MovimientosService) { }
 
-    @MessagePattern({ cmd: 'crear_movimiento_logistica' })
-    async crear(@Payload() data: CreateMovimientoDto) {
-        return await this.movimientosService.registrarMovimiento(data);
+    @MessagePattern({ cmd: 'crear_movimiento' }) // Asegúrate de que los espacios y comillas coincidan
+    async create(@Payload() createMovimientoDto: CreateMovimientoDto) {
+        console.log('Microservicio Logística: Recibido comando crear_movimiento', createMovimientoDto);
+        return await this.movimientosService.registrarMovimiento(createMovimientoDto);
     }
-
     @MessagePattern({ cmd: 'obtener_movimientos_producto' })
     async obtenerPorProducto(@Payload() data: { id_producto: string }) {
         return await this.movimientosService.buscarPorProducto(data.id_producto);
