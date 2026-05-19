7d602dfcc29f56fe2ccbf489e2ec968dff930475
jorgeasca <jascola171@gamil.com>
2026-05-19 11:41:57 +0200

    Gateway

--- apps/ms-gateway/src/modulos/ms-administracion/ms-administracion.controller.ts
+++ apps/ms-gateway/src/modulos/ms-administracion/ms-administracion.controller.ts
@@ -1,19 +1,17 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { 
-  CreateEmpresaDto, 
-  ADMIN_PATTERNS 
-} from '@app/common';
+import { ADMIN_PATTERNS } from '@app/common';
 
 @Controller('administracion') 
 export class AdministracionController {
   constructor(
-    @Inject('ADMINISTRACION_SERVICE') private readonly adminClient: ClientProxy,
+    // CAMBIA ESTO: De 'ADMINISTRACION_SERVICE' a 'NATS_SERVICE'
+    @Inject('NATS_SERVICE') private readonly natsClient: ClientProxy,
   ) {}
 
   @Post('empresas')
-  createEmpresa(@Body() createEmpresaDto: CreateEmpresaDto) {
-    
-    return this.adminClient.send(ADMIN_PATTERNS.CREAR_EMPRESA, createEmpresaDto);
+  createEmpresa(@Body() createEmpresaDto: any) {
+    // Usa natsClient en lugar de adminClient
+    return this.natsClient.send(ADMIN_PATTERNS.CREAR_EMPRESA, createEmpresaDto);
   }
 }
