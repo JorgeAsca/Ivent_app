ea885dd7b635cdd0615b48466b0c3947123d786d
jorgeasca <jascola171@gamil.com>
2026-05-19 11:54:09 +0200

    Gateway

--- apps/ms-gateway/src/modulos/ms-usuarios/ms-usuarios.controller.ts
+++ apps/ms-gateway/src/modulos/ms-usuarios/ms-usuarios.controller.ts
@@ -3,20 +3,22 @@ import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('usuarios')
 export class UsuariosController {
-    constructor(@Inject('MS_USUARIOS_SERVICE') private client: ClientProxy) { }
+    // CAMBIA ESTO: De 'MS_USUARIOS_SERVICE' a 'NATS_SERVICE'
+    constructor(@Inject('NATS_SERVICE') private readonly natsClient: ClientProxy) { }
 
     @Post()
     crearUsuario(@Body() data: any) {
-        return this.client.send({ cmd: 'crear_usuario' }, data);
+        // Usa natsClient
+        return this.natsClient.send({ cmd: 'crear_usuario' }, data);
     }
 
     @Post('roles')
     crearRol(@Body() data: any) {
-        return this.client.send({ cmd: 'crear_rol' }, data);
+        return this.natsClient.send({ cmd: 'crear_rol' }, data);
     }
 
     @Post('roles/asignar-permiso')
     asignarPermiso(@Body() data: { id_rol: string, id_permiso: string }) {
-        return this.client.send({ cmd: 'asignar_permiso_a_rol' }, data);
+        return this.natsClient.send({ cmd: 'asignar_permiso_a_rol' }, data);
     }
 }
